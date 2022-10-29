c5076608d23acfdac3537f3dad0b526254da3781
Nelson Soto <nelsonsoto22@gmail.com>
2022-10-29 12:40:43 -0400

    Adding CryptoHelper

--- PropertiesOnEarthAPI/Helpers/EncryptionHelper.cs
+++ PropertiesOnEarthAPI/Helpers/EncryptionHelper.cs
@@ -1,12 +1,19 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using CryptoHelper;
 
 namespace PropertiesOnEarthAPI.Helpers
 {
     public class EncryptionHelper
     {
-        
+        // Method for hashing the password
+        public string HashPassword(string password)
+        {
+            return Crypto.HashPassword(password);
+        }
+        // Method to verify the password hash against the given password
+        public bool VerifyPassword(string hash, string password)
+        {
+            return Crypto.VerifyHashedPassword(hash, password);
+        }
+
     }
 }
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # PropertiesOnEarth
 Web API for the Properties on earth System
+
+## Nugget Package Used
+
+- CriptoHelper
+```sh
+dotnet add package CryptoHelper --version 3.0.2
+```
+
+
