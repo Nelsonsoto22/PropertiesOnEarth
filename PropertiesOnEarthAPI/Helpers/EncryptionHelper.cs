863e05d0a9f5bd26429e6d9a0166a93f547e61bc
Nelson Soto <nelsonsoto22@gmail.com>
2022-10-29 12:55:44 -0400

    Adding Cryptography Lib

--- PropertiesOnEarthAPI/Helpers/EncryptionHelper.cs
+++ PropertiesOnEarthAPI/Helpers/EncryptionHelper.cs
@@ -1,4 +1,6 @@
+using System.Text;
 using CryptoHelper;
+using Konscious.Security.Cryptography;
 
 namespace PropertiesOnEarthAPI.Helpers
 {
@@ -13,6 +15,29 @@ namespace PropertiesOnEarthAPI.Helpers
         public bool VerifyPassword(string hash, string password)
         {
             return Crypto.VerifyHashedPassword(hash, password);
+        }
+        private bool VerifyHash(string password, byte[] salt, byte[] hash)
+        {
+            var newHash = HashPassword(password, salt);
+            return hash.SequenceEqual(newHash);
+        }
+        private byte[] CreateSalt()
+        {
+            var buffer = new byte[16];
+            var rng = new RNGCryptoServiceProvider();
+            rng.GetBytes(buffer);
+            return buffer;
+        }
+        private byte[] HashPassword(string password, byte[] salt)
+        {
+            var argon2 = new Argon2id(Encoding.UTF8.GetBytes(password));
+
+            argon2.Salt = salt;
+            argon2.DegreeOfParallelism = 8; // four cores
+            argon2.Iterations = 4;
+            argon2.MemorySize = 1024 * 1024; // 1 GB
+
+            return argon2.GetBytes(16);
         }
 
     }
--- README.md
+++ README.md
@@ -3,9 +3,12 @@ Web API for the Properties on earth System
 
 ## Nugget Package Used
 
-- CriptoHelper
+- Cryptography
 ```sh
 dotnet add package CryptoHelper --version 3.0.2
 ```
 
+```sh
+dotnet add package Konscious.Security.Cryptography.Argon2 --version 1.3.0
+```
 
